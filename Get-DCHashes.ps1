17f81bcd5c5d938213e26f2a3287a3b977a53a3c
idchoppers <54718446+idchoppers@users.noreply.github.com>
2024-06-14 20:13:32 -0400

    Update Get-DCHashes.ps1

--- Get-DCHashes.ps1
+++ Get-DCHashes.ps1
@@ -4,6 +4,9 @@
     Originally a tool for bruteforcing, but John does this way better, so I will let him take the wheel in that department.
     Tool takes three arguments: Domain Controller address, Naming Context, and file to output to
     Tool outputs: All account password hashes in the given target in John the Ripper format
+
+    Copyright 2022 Kyle Smith
+    Licensed under GPL-3.0-or-later
 #>
 
 Param (
